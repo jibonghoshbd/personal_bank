2b73d7c911b7afc2a500b0d10d9680f4a5dfe685
jibonghoshbd <jibonghosh1510@gmail.comm>
2022-02-18 02:22:12 +0600

    banking js connect

---
+++ js/banking.js
@@ -0,0 +1,12 @@
+const depositBtn = document.getElementById('deposit-btn');
+const withdrawBtn = document.getElementById('withdraw-btn');
+
+// handel deposti btn 
+depositBtn.addEventListener('click', function () {
+    console.log('hello deiposit')
+})
+
+// handel withdraw btn 
+withdrawBtn.addEventListener('click', function () {
+    console.log('hello withdraw')
+})
