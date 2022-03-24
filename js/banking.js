8c2d49d0ecba382bd715eb1cd3f16cfc9174c32f
jibonghoshbd <jibonghosh1510@gmail.comm>
2022-03-24 11:48:49 +0600

    withdraw and deposit calculation clear

--- js/banking.js
+++ js/banking.js
@@ -3,10 +3,48 @@ const withdrawBtn = document.getElementById('withdraw-btn');
 
 // handel deposti btn 
 depositBtn.addEventListener('click', function () {
-    console.log('hello deiposit')
+    // deposit input value 
+    const depositInput = document.getElementById('deposit-input');
+    const depositAmountText = depositInput.value;
+    const depositAmount = parseFloat(depositAmountText)
+    // console.log(depositAmount)
+
+    //update total deposit  
+    const depositTotal = document.getElementById('deposit-total');
+    const depositTotaltext = depositTotal.innerText;
+    const depositTotalAmount = parseFloat(depositTotaltext)
+    depositTotal.innerText = depositAmount + depositTotalAmount;
+
+    // update blance total  
+    const blanceTotal = document.getElementById('blance-total');
+    const blanceTotalText = blanceTotal.innerText;
+    const previousBlanceTotal = parseFloat(blanceTotalText)
+    blanceTotal.innerText = previousBlanceTotal + depositAmount;
+
+    // clear deposit input value  
+    depositInput.value = '';
+
 })
 
 // handel withdraw btn 
 withdrawBtn.addEventListener('click', function () {
-    console.log('hello withdraw')
+    // withdraw input 
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmountText = withdrawInput.value;
+    const withdrawAmount = parseFloat(withdrawAmountText);
+
+    // update withdraw total 
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const withdrawTotalText = withdrawTotal.innerText;
+    const withdrawTotalAmount = parseFloat(withdrawTotalText);
+    withdrawTotal.innerText = withdrawTotalAmount + withdrawAmount;
+
+    // update blance total 
+    const blanceTotal = document.getElementById('blance-total');
+    const blanceTotalText = blanceTotal.innerText;
+    const previousBlanceTotal = parseFloat(blanceTotalText)
+    blanceTotal.innerText = previousBlanceTotal - withdrawAmount;
+
+    // clear withdraw input 
+    withdrawInput.value = '';
 })
